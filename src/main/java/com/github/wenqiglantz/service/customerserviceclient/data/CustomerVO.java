954d5656fbb274da91b90f49c4ddf5fec43fcda2
wenqiglantz <wenqiglantz@gmail.com>
2022-10-23 07:07:26 -0400

    initial commit for spring boot 3 upgrade

--- src/main/java/com/github/wenqiglantz/service/customerserviceclient/data/CustomerVO.java
+++ src/main/java/com/github/wenqiglantz/service/customerserviceclient/data/CustomerVO.java
@@ -6,8 +6,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Data
 @Builder
